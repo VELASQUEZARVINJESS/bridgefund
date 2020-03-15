9921acdbe4ebdf4f54213b81ad448b3a5484914b
mars <marscreation21@gmail.com>
2020-03-15 20:02:31 +0800

    add collection report

---
+++ app/view/java/reports/collection.java.php
@@ -0,0 +1,65 @@
+<script>
+	$(()=>{
+		$('.content-wrapper').css('background','#000');
+		$('div.card-body').css('background','#111');
+		$('h1.pageTitle').css('color','#FFF');
+		$('footer.main-footer').css({'color':'#FFF','background':'#131313','border':0});
+		$('input').css({'color':'#FFF','background':'#242424','border':0});
+		$('div.modal-content').css({'background':'#111','color':'#FFF'});
+		$('div.modal-content > div').css({'border-bottom-color':'#242424'});
+		$('table').addClass('table-dark');
+		$('div.card').css({'color':'white'});
+		$('div.card .card-header').css({'background-color':'#000'});
+		$('body').css({'background':'black'});
+
+		$('input.colsearch').daterangepicker({
+			timePicker: false,
+			minDate: new Date('2020-02-01'),
+			maxDate: moment(),
+			locale: { format: 'MMM DD, YYYY' },
+			autoApply: true
+		});
+		$('button.btn-search').click(function() {
+			getCollection();
+		});
+	});
+	pageTitle('Collection Report');
+
+	function getCollection() {
+		let data = {
+			sdate: formatDate(new Date($('input.colsearch').data('daterangepicker').startDate)),
+			edate: formatDate(new Date($('input.colsearch').data('daterangepicker').endDate))
+		}
+		let tbl = $('div.collection table');
+		tbl.find('tbody').html('');
+		$.ajax({
+			type: 'POST',
+			dataType: 'JSON',
+			url: '<?php echo $req;?>',
+			data: { part: 'getCollection', data: data},
+			success: function(d) {
+				if (typeof d == 'object') {
+					d.forEach(function(v) {
+						let count = v.duration;
+						if (d.cycle == 'Bimonthly') {
+							count = parseInt(count) + 2;
+						}
+						tbl.find('tbody').append($('<tr/>')
+							.append($('<td/>').text(v.date))
+							.append($('<td/>').text(v.name))
+							.append($('<td/>').text(v.loanid))
+							.append($('<td/>').text(v.method))
+							.append($('<td/>').text(formatCurrency(v.amount)))
+							.append($('<td/>').text(formatCurrency(v.penalty)))
+							.append($('<td/>').text(v.term + ' / ' + count))
+						);
+					});
+				}
+			},
+			error: function(x) {
+				console.log(x.reponseText);
+			}
+		});
+	}
+
+</script>
---
+++ app/view/page/reports/collection.php
@@ -0,0 +1,33 @@
+<div class="collection">
+	<div class="card card-primary">
+		<div class="card-body">
+			<div class="row mb-2">
+				<div class="col-md-4">
+					<div class="input-group">
+						<input type="text" class="form-control colsearch">
+						<span class="input-group-append">
+							<button type="button" class="btn btn-primary btn-flat btn-search"><i class="fas fa-search"></i></button>
+						</span>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<table class="table table-sm ml-2 lr-2">
+					<thead>
+						<th>Date</th>
+						<th>Name</th>
+						<th>Loan #</th>
+						<th>Payment Type</th>
+						<th>Collection</th>
+						<th>Penalty</th>
+						<th>Term</th>
+					</thead>
+					<tbody></tbody>
+				</table>
+			</div>
+		</div>
+	</div>
+	<style>
+		div.collection table th, div.collection table td  { text-align: center;}
+	</style>
+</div>
