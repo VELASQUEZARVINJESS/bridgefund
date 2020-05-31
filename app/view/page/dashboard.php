c01eeffc1151b94bd16ce822d090d3b3bb738080
Jess <velasquezarvinjess@gmail.com>
2020-06-01 06:30:44 +0800

    dashbaord

---
+++ app/view/page/dashboard.php
@@ -0,0 +1,150 @@
+<div class="content">
+    <div class="section">
+        <div class="row">
+
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="icon">
+                                    <i class="fas fa-users fa-2x " style="color:#34495e;"></i>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <small class="card-subtitle text-muted float-right">Barrowers</small><br>
+                                <h5 class="card-title float-right">0</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="icon">
+                                    <i class="fas fa-file-invoice-dollar fa-2x " style="color:#34495e;"></i>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <small class="card-subtitle text-muted float-right">Loans</small><br>
+                                <h5 class="card-title float-right">0</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="icon">
+                                    <i class="fas fa-bell fa-2x " style="color:#34495e;"></i>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <small class="card-subtitle text-muted float-right">Penalty</small><br>
+                                <h5 class="card-title float-right">0</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="card">
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-6">
+                                <div class="icon">
+                                    <i class="fas fa-piggy-bank fa-2x " style="color:#34495e;"></i>
+                                </div>
+                            </div>
+                            <div class="col-6">
+                                <small class="card-subtitle text-muted float-right">Bank</small><br>
+                                <h5 class="card-title float-right">0</h5>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div><!-- ./row -->
+    </div><!-- ./section -->
+
+    <div class="section">
+        <div class="row">
+            <div class="col-9">
+                <div class="card">
+                    <div class="card-header">
+                         <small class="card-title ">Options</small>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                                 <i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                    </div>
+                   <div class="card-body">
+                        <div class="row">
+                            
+                        </div>
+                   </div>
+                </div>
+            </div>
+
+            <div class="col-3">
+                <div class="col-12">
+                    <div class="card bg-gradient-secondary ">
+                        <div class="card-body"> 
+                             <div class="row">
+                                <div class="col-6">
+                                    <small class="card-subtitle text-success"> <strong>Approve</strong></small>
+                                    </div>
+                                 <div class="col-6">
+                                    <small class="card-title float-right">0</small>
+                                 </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="card bg-gradient-secondary">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-6">
+                                    <small class="card-subtitle text-warning"> <strong>Pending</strong></small>
+                                    </div>
+                                <div class="col-6">
+                                    <small class="card-title float-right">0</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="col-12">
+                    <div class="card bg-gradient-secondary">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-6">
+                                    <small class="card-subtitle text-danger"> <strong>Decline</strong></small>
+                                </div>
+                                <div class="col-6">
+                                    <small class="card-title float-right">0</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div><!-- ./row -->
+    </div><!-- ./section -->
+
+</div><!-- ./content -->
